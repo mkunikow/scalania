f1babd6c4f83e80ab2cb3d1f46aeb238876f1cc2
mkunikowski <michal.kunikowski@gmail.com>
2015-11-29 23:11:10 +0100

    P02

--- exercises/src/main/scala/pl/japila/scalania/s99/S99_P02.scala
+++ exercises/src/main/scala/pl/japila/scalania/s99/S99_P02.scala
@@ -1,5 +1,11 @@
 package pl.japila.scalania.s99
 
 object S99_P02 {
-  def penultimate[T](ts: Seq[T]): Option[T] = ???
+  def penultimate[T](ts: Seq[T]): Option[T] = {
+    if (ts.size > 1) {
+      ts.takeRight(2).headOption
+    } else {
+      None
+    }
+  }
 }
